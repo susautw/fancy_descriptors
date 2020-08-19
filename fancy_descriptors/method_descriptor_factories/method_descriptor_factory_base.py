a332a13474b9f0f36ebf1603acbb16db53670105
susautw <susautw@gmail.com>
2020-08-19 11:34:23 +0800

    Move: patterns into fancy_descriptors

--- fancy_descriptors/method_descriptor_factories/method_descriptor_factory_base.py
+++ fancy_descriptors/method_descriptor_factories/method_descriptor_factory_base.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Callable
 
 from fancy_descriptors.method_descriptor_base import MethodDescriptorBase
-from patterns import Singleton
+from fancy_descriptors.patterns import Singleton
 
 
 class MethodDescriptorFactoryBase(ABC, Singleton):
