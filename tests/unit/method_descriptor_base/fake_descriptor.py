4a2aad99b0774b975169825a0a4580286bb808b2
susautw <susautw@gmail.com>
2020-08-19 10:52:47 +0800

    refine test

--- tests/unit/method_descriptor_base/fake_descriptor.py
+++ tests/unit/method_descriptor_base/fake_descriptor.py
@@ -1,13 +1,14 @@
 from typing import Any, Callable
 
 from fancy_descriptors.method_descriptor_base import MethodDescriptor
+from fancy_descriptors.method_descriptor_factories import MethodDescriptorFactoryBase
 
 
 class FakeDescriptor(MethodDescriptor):
     value: Any
 
-    def __init__(self, method: Callable, value: Any):
-        super().__init__(method)
+    def __init__(self, method: Callable, value: Any, factory: MethodDescriptorFactoryBase = None):
+        super().__init__(method, factory=factory)
         self.value = value
 
     def get_value(self):
